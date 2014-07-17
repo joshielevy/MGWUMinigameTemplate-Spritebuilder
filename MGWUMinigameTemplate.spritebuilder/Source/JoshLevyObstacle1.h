1aecde9ac4e4be15f8e5ef3bee20e264db1fc0b0
Josh Levy <joshlevy@Josh-Levys-MacBook-Pro.local>
2014-07-17 17:35:10 -0400

    working on obstacles and animating them

---
+++ MGWUMinigameTemplate.spritebuilder/Source/JoshLevyObstacle1.h
@@ -0,0 +1,13 @@
+//
+//  JoshLevyObstacle1.h
+//  MGWUMinigameTemplate
+//
+//  Created by Josh Levy on 7/17/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface JoshLevyObstacle1 : CCNode
+
+@end
